feb02af5395704d17782100bf32f02f02f32ffa4
Aiden Van Dyke <aidenavandyke@gmail.com>
2026-09-23 09:57:39 -0600

    Add WIP scansat patcher files

---
+++ SCANsat/Setup.bat
@@ -0,0 +1,25 @@
+@echo off&setlocal
+
+:: Edit these if needed to point towards the correct location for your system
+:: Make sure not to insert any spaces around the equals sign
+set "KSP_GameData_Folder=C:\Steam\steamapps\common\Kerbal Space Program\GameData"
+set "Texconv_Path=C:\Program Files\Texconv\texconv.exe"
+
+where python > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: Python was not found in your system's PATH.
+    echo Please install python, or if it is installed, add it to the PATH system environmental variable.
+    pause
+    exit /b
+)
+
+for %%i in ("%~dp0..\") do set "batch_file_parent_directory=%%~fi"
+set "script_location=%batch_file_parent_directory%_SetupScript\setup.py"
+
+python "%script_location%" --mod-folder="%~dp0." --gamedata-folder="%KSP_GameData_Folder%" --texconv-path="%Texconv_Path%"
+
+IF %ERRORLEVEL% NEQ 0 (
+    PAUSE
+) ELSE (
+    ping 192.0.2.0 -n 2 -w 500>nul
+)
